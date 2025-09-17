459f2e57a57a2c07f3ec4927ab4985f905899410
livyajasmine <livyajasmine140@gmail.com>
2025-09-17 21:01:39 +0530

    commit

--- studentservice/src/main/java/com/tnsif/customerservice/CustomerApplicationTests.java
+++ studentservice/src/main/java/com/tnsif/customerservice/CustomerApplicationTests.java
@@ -1,13 +1,1 @@
-package com.tnsif.customerservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CustomerserviceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
